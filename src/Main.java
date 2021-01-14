7035d72954204cce83c5bd2b4ee0613f8b144fda
Дмитрий Асаторов <dmitrijasatorov@MacBook-Air-Dmitrij.local>
2021-01-14 15:02:43 +0300

    first commit

---
+++ src/Main.java
@@ -0,0 +1,8 @@
+public class Main {
+    public static void main(String[] args) {
+        // стоимость билета в копейках
+        long ticketcost = 15_500_000;
+        long bonus = ticketcost / 20 / 100;
+        System.out.println(bonus);
+    }
+}
